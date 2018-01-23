77bc04e9cd5f076406fbbe91f84f61f025b3e74e
m_7el <maisshelou@gmail.vom>
2018-01-23 11:24:05 +0200

    photoManager

--- app/src/main/java/com/example/m_7el/training/country/utils/PhotoManager.java
+++ app/src/main/java/com/example/m_7el/training/country/utils/PhotoManager.java
@@ -1,18 +1,23 @@
 package com.example.m_7el.training.country.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+
+import com.example.m_7el.training.R;
 
 /**
  * Created by m_7el on 1/22/2018.
  */
 
 public class PhotoManager {
-     public static  void  loadImage(Context context, ImageView imageView){
+     public static  void  loadImage(Context context, final ImageView imageView) {
          Glide.with(context).
                  load(ImageUrl.getURL())
+                 .error(R.drawable.ic_launcher_background)
                  .into(imageView);
 
      }
