450d4c7800a53f71051b8ea28de2a7cd4272f090
m_7el <maisshelou@gmail.vom>
2018-02-01 15:05:02 +0200

    delete GetCountryWeather class

--- app/src/main/java/com/example/m_7el/training/country/TomorrowFragment.java
+++ app/src/main/java/com/example/m_7el/training/country/TomorrowFragment.java
@@ -3,6 +3,7 @@ package com.example.m_7el.training.country;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -17,8 +18,9 @@ import com.example.m_7el.training.country.models.CountryInfo;
 import com.example.m_7el.training.country.models.Info;
 import com.example.m_7el.training.country.models.Main;
 import com.example.m_7el.training.country.models.WeatherDetails;
-import com.example.m_7el.training.country.utils.GetCountryWeather;
 import com.example.m_7el.training.country.utils.LogMessages;
+import com.example.m_7el.training.net.clients.RetrofitInterface;
+import com.example.m_7el.training.net.clients.WeatherApiClient;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,10 +28,16 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class TomorrowFragment extends Fragment implements WeatherFragment.CallBacks {
 
 
+    private final static String API_KEY = "1867722b6af87e1d0388e10c5a94be34";
+    private Info weatherInfo;
     private List<WeatherDetails> weatherDetails;
     private TextView date, humidity, pressure, temp;
     private String tomorrowDate;
@@ -59,14 +67,17 @@ public class TomorrowFragment extends Fragment implements WeatherFragment.CallBa
             mCountryInfo = savedInstanceState.getParcelable("country");
             weatherDetails = savedInstanceState.getParcelableArrayList("weather");
             tomorrowDate = savedInstanceState.getString("date");
-            setData(weatherDetails);
+            setData();
+
         }
+
         return view;
     }
 
     @Override
     public void onSelectedFragment(CountryInfo mCountryInfo) {
         this.mCountryInfo = mCountryInfo;
+
         Date now = new Date();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(now);
@@ -74,19 +85,38 @@ public class TomorrowFragment extends Fragment implements WeatherFragment.CallBa
         @SuppressLint("SimpleDateFormat") SimpleDateFormat mdformat = new SimpleDateFormat("yyyy-MM-dd");
         tomorrowDate = mdformat.format(calendar.getTime());
         if (mCountryInfo.getLatlng().size() != 0) {
-            Info weatherInfo = GetCountryWeather.getWeather(mCountryInfo);
-            if (weatherInfo != null) {
-                List<WeatherDetails> weatherDetails = weatherInfo.getWeatherDetails();
-                this.weatherDetails=weatherDetails;
-                setData(weatherDetails);
-            }
+            Call<Info> call2 = WeatherApiClient.getClient().create(RetrofitInterface.class).getWeatherInfo(mCountryInfo.getLatlng().get(0), mCountryInfo.getLatlng().get(1), API_KEY);
+            call2.enqueue(new Callback<Info>() {
+
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void onResponse(@NonNull Call<Info> call, @NonNull Response<Info> response) {
+
+                    weatherInfo = response.body();
+                    if (weatherInfo != null) {
+                        weatherDetails = weatherInfo.getWeatherDetails();
+                    }
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            setData();
+                        }
+                    }, 1000);
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<Info> call, @NonNull Throwable t) {
+                    t.getStackTrace();
+                }
+            });
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    private void setData(List<WeatherDetails>weatherDetails) {
 
+    @SuppressLint("SetTextI18n")
+    private void setData() {
         for (int i = 0; i < weatherDetails.size(); i++) {
+
             String[] neww = weatherDetails.get(i).getDtTxt().split(" ");
             if (neww[0].equals(tomorrowDate)) {
                 date.setText(tomorrowDate);
@@ -107,5 +137,6 @@ public class TomorrowFragment extends Fragment implements WeatherFragment.CallBa
         outState.putParcelable("country", mCountryInfo);
         outState.putParcelableArrayList("weather", (ArrayList<? extends Parcelable>) weatherDetails);
         outState.putString("date", tomorrowDate);
+
     }
 }
