48fb8b98416bd75012f56e575e0ae3c1750eb990
m_7el <maisshelou@gmail.vom>
2018-02-07 16:31:50 +0200

    refresh recyclerView

---
+++ app/src/main/java/com/example/m_7el/training/country/WeatherDayInfoListener.java
@@ -0,0 +1,8 @@
+package com.example.m_7el.training.country;
+
+/**
+ * Created by m_7el on 2/7/2018.
+ */
+
+public interface WeatherDayInfoListener {
+}
--- app/src/main/java/com/example/m_7el/training/country/WeatherViewPagerAdapter.java
+++ app/src/main/java/com/example/m_7el/training/country/WeatherViewPagerAdapter.java
@@ -3,6 +3,7 @@ package com.example.m_7el.training.country;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.example.m_7el.training.country.models.Info;
 
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class WeatherViewPagerAdapter extends FragmentPagerAdapter implements WeatherFragment.SelectedFragmentListener {
+public class WeatherViewPagerAdapter extends FragmentStatePagerAdapter implements WeatherFragment.SelectedFragmentListener {
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
 
